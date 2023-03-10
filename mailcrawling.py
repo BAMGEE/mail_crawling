45836749fa0089030a99b4026741fa4036ef1d77
BAMGEE <bamgee@uosarch.ac.kr>
2023-03-10 18:45:28 +0900

    modify class

--- mailcrawling.py
+++ mailcrawling.py
@@ -26,76 +26,68 @@ ws = wb.active
 
 # -------------- Class
 class Site:
-    idx = 1
     list = []
-    ws = None
+    
+    def __init__(self, ws, idx):
+        self.ws = ws
+        self.idx = idx
+    
+    # -------------- Mail Crawling Function
+    def MailCrawling(self):
+        # Viewing Progress
+        ViewingProgress()
+        
+        # The arrow object to get the received time.
+        arrowobj = arrow.get(mail.ReceivedTime)
+        
+        # Col A: Numbering
+        self.ws[f'A{self.idx}'] = self.idx
+        # Col B: Received datetime(set 'tzinfo' to 'None')
+        self.ws[f'B{self.idx}'] = arrowobj.datetime.replace(tzinfo=None)
+        # Col C: Sender Name(Sender Email Address)
+        if mail.Class == 43 and mail.SenderEmailType == 'EX' and mail.Sender.GetExchangeUser() != None:
+            self.ws[f'C{self.idx}'] = mail.SenderName + "(" + mail.Sender.GetExchangeUser().PrimarySmtpAddress + ")"
+        else:
+            self.ws[f'C{self.idx}'] = mail.SenderName + "(" + mail.SenderEmailAddress + ")"
+        # Col D: receiving email addresse
+        self.ws[f'D{self.idx}'] = mail.To
+        # Col E: Mail Title
+        self.ws[f'E{self.idx}'] = mail.Subject
+        # Col F: Mail Body
+        self.ws[f'F{self.idx}'] = mail.Body
+        
+        # Get the mail attachments
+        attachments = mail.Attachments
+        # Mail attachments' count
+        r = attachments.count
+        # Loop to save attachments (File Name: MailNumbering_AttachmentCount_AttachmentTitle)
+        for j in range(1, r+1):
+            attachment = attachments.Item(j)
+            attachment.SaveASFile(AttachPath + str(self.idx) + "_" + str(j) + "_" + str(attachment)) # File Name
+        return self.idx
 
 # -------------- Site
-kb = Site()
-kb.idx = 1
-kb.ws = wb.create_sheet('KB', 0)
+kb = Site(wb.create_sheet('KB', 0), 1)
 kb.list = ['KB', 'kb']
 
-bnk = Site()
-bnk.idx = 1
-bnk.ws = wb.create_sheet('BNK', 1)
+bnk = Site(wb.create_sheet('BNK', 1), 1)
 bnk.list = ['BNK', 'bnk']
 
-hana = Site()
-hana.idx = 1
-hana.ws = wb.create_sheet('하나', 2)
+hana = Site(wb.create_sheet('하나', 2), 1)
 hana.list = ['하나금융', '하나투자']
 
-yt = Site()
-yt.idx = 1
-yt.ws = wb.create_sheet('유안타', 3)
+yt = Site(wb.create_sheet('유안타', 3), 1)
 yt.list = ['유안타', 'yuanta']
 
-eb = Site()
-eb.idx = 1
-eb.ws = wb.create_sheet('이베스트', 4)
+eb = Site(wb.create_sheet('이베스트', 4), 1)
 eb.list = ['이베스트', 'ebst']
 
 # -------------- File Path
 SavePath = "C:\\Users\\gee\\Desktop\\"
 AttachPath = "C:\\Users\\gee\\Desktop\\attachments\\"
 
-# -------------- Mail Crawling Function
-def MailCrawling(ws, i):
-    # Viewing Progress
-    ViewingProgress(i)
-    
-    # The arrow object to get the received time.
-    arrowobj = arrow.get(mail.ReceivedTime)
-    
-    # Col A: Numbering
-    ws[f'A{i}'] = i
-    # Col B: Received datetime(set 'tzinfo' to 'None')
-    ws[f'B{i}'] = arrowobj.datetime.replace(tzinfo=None)
-    # Col C: Sender Name(Sender Email Address)
-    if mail.Class == 43 and mail.SenderEmailType == 'EX' and mail.Sender.GetExchangeUser() != None:
-        ws[f'C{i}'] = mail.SenderName + "(" + mail.Sender.GetExchangeUser().PrimarySmtpAddress + ")"
-    else:
-        ws[f'C{i}'] = mail.SenderName + "(" + mail.SenderEmailAddress + ")"
-    # Col D: receiving email addresse
-    ws[f'D{i}'] = mail.To
-    # Col E: Mail Title
-    ws[f'E{i}'] = mail.Subject
-    # Col F: Mail Body
-    ws[f'F{i}'] = mail.Body
-    
-    # Get the mail attachments
-    attachments = mail.Attachments
-    # Mail attachments' count
-    r = attachments.count
-    # Loop to save attachments (File Name: MailNumbering_AttachmentCount_AttachmentTitle)
-    for j in range(1, r+1):
-        attachment = attachments.Item(j)
-        attachment.SaveASFile(AttachPath + str(i) + "_" + str(j) + "_" + str(attachment)) # File Name
-    return i
-
 # -------------- Viewing Progress Function
-def ViewingProgress(i):
+def ViewingProgress():
     os.system('cls')
     print("In progress... (" + str((kb.idx-1)+(bnk.idx-1)+(hana.idx-1)+(yt.idx-1)+(eb.idx-1)+skip) + "/" + str(msg_count)+ ")")
 
@@ -104,22 +96,22 @@ if __name__ == '__main__':
     # -------------- Mail Crawling Filtering Kewords
     for mail in messages:
         if any(str in mail.Subject + mail.Body for str in kb.list):
-            kb.idx = MailCrawling(kb.ws, kb.idx)
+            kb.idx = kb.MailCrawling()
             kb.idx += 1
         elif any(str in mail.Subject + mail.Body for str in bnk.list):
-            bnk.idx = MailCrawling(bnk.ws, bnk.idx)
+            bnk.idx = bnk.MailCrawling()
             bnk.idx += 1
         elif any(str in mail.Subject + mail.Body for str in hana.list):
-            hana.idx = MailCrawling(hana.ws, hana.idx)
+            hana.idx = hana.MailCrawling()
             hana.idx += 1
         elif any(str in mail.Subject + mail.Body for str in yt.list):
-            yt.idx = MailCrawling(yt.ws, yt.idx)
+            yt.idx = yt.MailCrawling()
             yt.idx += 1
         elif any(str in mail.Subject + mail.Body for str in eb.list):
-            eb.idx = MailCrawling(eb.ws, eb.idx)
+            eb.idx = eb.MailCrawling()
             eb.idx += 1
         else:
-            ViewingProgress(skip)
+            ViewingProgress()
             skip += 1
             
     # -------------- Save Excel File
